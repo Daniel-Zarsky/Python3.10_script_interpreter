e9743fe7b02824a6c69605572945e421afe9a679
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2023-04-04 11:51:58 +0200

    feat: instructions are sorted in list

--- interpret.py
+++ interpret.py
@@ -5,11 +5,50 @@ import sys
 import os
 
 
+class Operand:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def get_type(self):
+        return self.type
+
+    def get_value(self):
+        return self.value
+
+
+class Instruction:
+    def __init__(self, order, opcode, operands, frame_stack, labels, input_file, in_list):
+        self.order = order
+        self.opcode = opcode
+        self.operands = operands
+        self.frames = frame_stack
+        self.labels = labels
+        self.input = input_file
+        self.list = in_list
+        self.expected = None  # expected types of arguments
+
+    def check_operands(self):
+
+        for i, o in enumerate(self.operands, 1):
+            if self.expected[i] != self.operands[i].type:
+                print("wrong operand type", file=sys.stderr)
+                exit(1)  # todo
+
+    def debug(self):
+        for op in self.operands:
+            if op is not None:
+                print(op.type)
+                print(op.value)
+
+
 class Read_source:
-    def __init__(self, source_file, in_list):
+    def __init__(self, source_file, stack, labels, input_file):
         self.root = None
         self.source = source_file
-        self.list = in_list
+        self.frame_stack = stack
+        self.labels = labels
+        self.input_file = input_file
 
     def load(self):
         try:
@@ -22,12 +61,59 @@ class Read_source:
     def check(self):
 
         try:
-            if self.root.attrib['language'].upper() != "IPPcode23".upper():
+            if self.root.attrib['language'].upper() != "IPPcode23".upper():  # name a description nekotroluju kdyz
+                # jsou volitelne
                 print("error while reading xml", file=sys.stderr)
                 exit(1)  # todo
         except KeyError:
             print("error while reading xml", file=sys.stderr)
             exit(1)  # todo
+
+        for child in self.root:
+            if child.tag != 'instruction':
+                print("chyba v xml", file=sys.stderr)
+                exit(1)
+
+            child_at = child.attrib
+            if len(child_at) != 2 or ('opcode' not in child_at or 'order' not in child_at):
+                print("chyba v xml", file=sys.stderr)
+                exit(1)
+
+            for number, arg in enumerate(child, 1):
+                try:
+                    if not re.match('^arg[123]$', arg.tag) or number > 3:
+                        print("spatny argument", file=sys.stderr)
+                        exit(1)
+                except KeyError:
+                    print("error while checking arguments", file=sys.stderr)
+                    exit(1)  # todo
+
+    def fill_list(self):
+
+        instruction_list = []
+        for child in self.root:
+            opcode = child.attrib['opcode']
+            operands = [None] * 3
+            order = child.attrib['order']
+            for i, subchild in enumerate(child, 1):
+                if subchild.tag == 'arg1':
+                    operands[0] = Operand(subchild.attrib['type'], subchild.text)
+                elif subchild.tag == 'arg2':
+                    operands[1] = Operand(subchild.attrib['type'], subchild.text)
+                elif subchild.tag == 'arg3':
+                    operands[2] = Operand(subchild.attrib['type'], subchild.text)
+                else:
+                    print("error while loading operands", file=sys.stderr)
+                    exit(1)
+
+                new_int = Instruction(order, opcode, operands, self.frame_stack, self.labels, self.input_file,
+                                      instruction_list)
+                instruction_list.append(new_int)
+
+        sorted_instructions = sorted(instruction_list, key=lambda x: x.order, reverse=False)
+
+        return sorted_instructions
+
 
 class Interpreter:
 
@@ -36,13 +122,23 @@ class Interpreter:
         self.input = input_file
         self.in_list = []
         self.in_cnt = 0
-        self.frame_cnt = []
+        self.frame_stack = []
         self.labels = {}  # dictionary
 
     def main(self):
-        xml = Read_source(self.source, self.in_list)
+        xml = Read_source(self.source, self.frame_stack, self.labels, self.in_list)
         xml.load()
         xml.check()
+        self.in_list = xml.fill_list()
+        i = 0
+        while i < len(self.in_list):
+
+            instruction = self.in_list[i]
+            if instruction is None:
+                break
+            else:
+                i = i + 1
+                instruction.debug()
 
 
 parser = argparse.ArgumentParser(
